830fe095b71999d010772b0eda812edd43a9e839
Mahyuddin <yudi.al@gmail.com>
2017-04-14 21:59:00 +0700

    Change product to ridon

--- ridon.mk
+++ ridon.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_gts210ltexx
+PRODUCT_NAME := ridon_gts210ltexx
 PRODUCT_DEVICE := gts210ltexx
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T815
